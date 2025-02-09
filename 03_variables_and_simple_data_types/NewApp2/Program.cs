7ba7b1ba12dfba7996f7ce155c592c48a962d575
Aliaksandr Kulba <tunet000@gmail.com>
2025-02-09 18:55:24 +0200

    feat: add lesson 3 examples

---
+++ 03_variables_and_simple_data_types/NewApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// целочисленные типы данных
+byte a = 15;        // 0 … 255
+sbyte b = -20;      // -128 … 127
+
+int с = -100000;    // -2 147 483 648 … 2 147 483 647
+uint d = 50000;     // 0 … 4 294 967 295
+
+
+//Console.WriteLine(a);
+//Console.WriteLine(a.GetType());
+
+// числа с плавающей запятой
+float pi = 3.14f;
+double pi2 = 3.1415926535;
+decimal pi3 = 3.141592653589793m;
+
+
+
+// логический тип
+bool l1 = true;
+bool l2 = false;
+
+
+
+// символьный тип
+char c1 = 'D';
+
+
+
+
+// строковый тип
+string str = "Hello, world!";
+
+
+var x = 50;
+
+Console.WriteLine(x);
+Console.WriteLine(x.GetType());
