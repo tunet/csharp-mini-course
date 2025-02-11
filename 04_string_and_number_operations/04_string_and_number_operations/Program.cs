6a54a48f8b191e51330c7f5559d4d08aab30c263
Aliaksandr Kulba <tunet000@gmail.com>
2025-02-11 20:47:15 +0200

    feat: add lesson 4

---
+++ 04_string_and_number_operations/04_string_and_number_operations/Program.cs
@@ -0,0 +1,89 @@
+﻿var userInput = Console.ReadLine();
+Console.WriteLine($"My input is: {userInput}");
+
+
+
+
+//var number = 10;
+////number++;
+//number--;
+//Console.WriteLine(number);
+
+
+
+
+//var str = "Hello";
+//Console.WriteLine(str);
+
+//str += ", World!";
+//Console.WriteLine(str);
+
+
+
+
+
+//var number = 10;
+//Console.WriteLine(number);
+
+//number += 2;
+////number = number + 2;
+//Console.WriteLine(number);
+
+
+
+
+//var number1 = 30;
+//var number2 = 20;
+
+//var string1 = "Hello";
+//var string2 = "World";
+
+//var result = $"{string1}, {string2}! Sum = {number1 + number2}";
+//Console.WriteLine(result);
+
+
+
+
+
+
+//var number1 = 30;
+//var string1 = "Hello";
+
+//var result = number1 + string1;
+//Console.WriteLine(result);
+
+
+
+
+
+
+
+//var string1 = "Hello, ";
+//var string2 = "World!";
+
+//var result = string1 + string2;
+//Console.WriteLine(result);
+
+
+
+
+
+//var number1 = 30;
+//var number2 = 20;
+
+//var sum = number1 + number2;
+//Console.WriteLine(sum);
+//Console.WriteLine();
+
+//var diff = number1 - number2;
+//Console.WriteLine(diff);
+//Console.WriteLine();
+
+//var product = number1 * number2;
+//Console.WriteLine(product);
+//Console.WriteLine();
+
+//var quotient = (float)number1 / (float)number2;
+//Console.WriteLine(quotient);
+//Console.WriteLine(quotient.GetType());
+//Console.WriteLine();
